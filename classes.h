36a328029cc30f1898250b54aa03b6a19d05bae2
arshiasamimi <samimarsh44@gmail.com>
2024-03-14 22:20:34 +0330

    adding methodes to character

--- classes.h
+++ classes.h
@@ -83,7 +83,7 @@ protected:
     LimitedStorage backpack;
     Stat hp;
     Stat stamina;
-    int fireArmLevel;
+    int firearmLevel;
     int meleeLevel;
     vector<Character *> currentWave;
     int coins;
@@ -93,24 +93,46 @@ public:
     string getName() const { return name; }
     int getAge() const { return age; }
     string getGender() const { return gender; }
-    int level() { return fireArmLevel + meleeLevel + hp.level() + stamina.level(); }
+    int level() { return firearmLevel + meleeLevel + hp.level() + stamina.level(); }
     LimitedStorage getBackpack() const { return backpack; }
-    vector<Character*> getWave() { return currentWave; }
+    vector<Character *> getWave() { return currentWave; }
+    int getFirearmLevel() { return firearmLevel; }
+    int getMeleeLevel() { return meleeLevel; }
+    int getCoins() { return coins; }
+
     // setters
-    void setName(const string &newName)
-    {
-        name = newName;
-    }
-    void setAge(int newAge)
-    {
-        age = newAge;
-    }
-    void setGender(const string &newGender)
-    {
-        gender = newGender;
-    }
+    void setName(const string &newName) { name = newName; }
+    void setAge(int newAge) { age = newAge; }
+    void setGender(const string &newGender) { gender = newGender; }
+    void setBackpack(LimitedStorage newBackpack) { backpack = newBackpack; }
+    void setWave(vector<Character *> newWave) { currentWave = newWave; }
+    void setFirearmLevel(int newLevel) { firearmLevel = newLevel; }
+    void setMeleeLevel(int newLevel) { meleeLevel = newLevel; }
+    void setCoins(int newCoins) { coins = newCoins; }
+
+    // others
+    virtual void turn() {}
+    virtual void death() {}
 };
 
-class Mission
+class Player : public Character
 {
+protected:
+    Storage *inventory;
+
+public:
+    // getters
+    Storage *getInventory() { return inventory; }
+    // others
+    virtual void turn(){}
+    virtual void death(){}
 };
+
+class Enemy: public Character{
+public:
+    virtual void turn (){}
+    virtual void death(){}
+};
+class HumanEnemy: public Enemy {};
+class ZombieEnemy: public Enemy {};
+class SpecialZombie: public ZombieEnemy {};
