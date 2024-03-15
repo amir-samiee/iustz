9296a0d82b155e0421a0b472708152775bdb9c0e
arshiasamimi <samimarsh44@gmail.com>
2024-03-15 21:26:10 +0330

    changing character uml to png

--- storage.cpp
+++ storage.cpp
@@ -11,12 +11,33 @@ protected:
 public:
     virtual void addItem(string name)
     {
+        if (items.find(name) == items.end())
+            items.insert({name, 1});
+        else
+            items[name]++;
     }
     void printStorage()
     {
+        int i = 1;
+        for (auto item : items)
+        {
+            cout<<i<<"-"<<dictionary[item.first].getName()<<" "<<dictionary[item.first];
+        }
     }
-    void removeItem(string itemName)
+    void removeItem(string name)
     {
+        while(true){
+            if (items[name] < 1){
+                cout<<"item does not exist.\n";
+            }
+            else if(items[name] == 1){
+                items.erase(name);
+                break;
+            }
+
+            else
+                items[name]--;
+        }
     }
 };
 class LimitedStorage : public Storage
