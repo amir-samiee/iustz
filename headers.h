eb74f4468e40ef541457f45d2b3533fa38c1318e
Amir Hossein Samiee <celmir4444@gmail.com>
2024-03-16 11:21:21 +0330

    minor changes

--- headers.h
+++ headers.h
@@ -2,14 +2,15 @@
 #ifndef HEADERS_H
 #define HEADERS_H
 
+#include <windows.h>
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <ctype.h>
 #include <vector>
 #include <map>
-#include <fstream>
-#include <windows.h>
-#include <ctype.h>
-#include<iomanip>
 
+// the order of the following lines matters. make changes wisely!
 #include "Assets/nlohmann/json.hpp"
 #include "helper.cpp"
 #include "item.cpp"
