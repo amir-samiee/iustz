d81531ec3a09626b6ad03805479ae171b67b7b94
Amir Hossein Samiee <celmir4444@gmail.com>
2024-03-16 23:04:21 +0330

    Major changes in headers system
    adding both .h and .cpp files for separated parts of the code

---
+++ character.h
@@ -0,0 +1,101 @@
+#pragma once
+#include "headers.h"
+
+class Stat
+{
+protected:
+    int maxPoint = 100;
+    int currentPoint = maxPoint;
+
+public:
+    // getters:
+    int getMaxPoint() const { return maxPoint; }
+    int getCurrentPoint() const { return currentPoint; }
+
+    // setters:
+    void setMaxPoint(int newValue) { maxPoint = newValue; }
+    void setCurrentPoint(int newValue);
+
+    int level();
+};
+
+class Character
+{
+protected:
+    string name;
+    int age;
+    string gender; // -male -female -other
+    LimitedStorage backpack;
+    Stat hp;
+    Stat stamina;
+    int firearmLevel = 1;
+    int meleeLevel = 1;
+    int powerBoost = 1;
+    vector<Character *> currentWave;
+    int coins;
+
+public:
+    // getters
+    string getName() const { return name; }
+    int getAge() const { return age; }
+    string getGender() const { return gender; }
+    LimitedStorage getBackpack() const { return backpack; }
+    Stat getHp() const { return hp; }
+    Stat getStamina() const { return stamina; }
+    vector<Character *> getWave() const { return currentWave; }
+    int getFirearmLevel() const { return firearmLevel; }
+    int getMeleeLevel() const { return meleeLevel; }
+    int getPowerBoost() const { return powerBoost; }
+    int getCoins() const { return coins; }
+
+    // setters
+    void setName(const string &newName) { name = newName; }
+    void setAge(int newAge) { age = newAge; }
+    void setGender(const string &newGender) { gender = newGender; }
+    void setBackpack(LimitedStorage newBackpack) { backpack = newBackpack; }
+    void setHp(Stat newHp) { hp = newHp; }
+    void setStamina(Stat newStamina) { stamina = newStamina; }
+    void setWave(vector<Character *> newWave) { currentWave = newWave; }
+    void setFirearmLevel(int newLevel) { firearmLevel = newLevel; }
+    void setMeleeLevel(int newLevel) { meleeLevel = newLevel; }
+    void setPowerBoost(int newPowerBoost) { powerBoost = newPowerBoost; }
+    void setCoins(int newCoins) { coins = newCoins; }
+
+    // others
+    int level();
+    virtual void turn() = 0;
+    virtual void die() = 0;
+};
+
+class Player : public Character
+{
+protected:
+    Storage inventory;
+    int humanLevels = 0;
+    int zombieLevels = 0;
+
+public:
+    // getters
+    Storage getInventory() const { return inventory; }
+    // others
+    void turn() override;
+    void die() override;
+};
+
+class Enemy : public Character
+{
+public:
+    void turn() override;
+    void die() override;
+};
+class HumanEnemy : public Enemy
+{
+};
+class ZombieEnemy : public Enemy
+{
+};
+class SpecialZombie : public ZombieEnemy
+{
+public:
+    void turn() override;
+};
--- main.cpp
+++ main.cpp
@@ -5,12 +5,6 @@ int main()
     clearScreen();
     cout << iustzTitle << string(2, '\n') << "Enter your usename: ";
     string username;
-    /*Storage storage;
-    storage.addItem("Axe");
-    storage.addItem("Punch");
-    storage.addItem("Grenade");
-    storage.addItem("Molotov Cocktails");
-    storage.printStorage();*/
     getline(cin, username);
     json users = loadData(usersFilePath);
     if (users[username].is_null())
@@ -21,16 +15,16 @@ int main()
         pprint("");
         pprint("Hello, friend.");
         pprint("This is the end of the world...");
-        pprint("The quick brown fox jumps over the lazy dog.");
-        pprint("In the midst of chaos, there is also opportunity.");
-        pprint("Life is like riding a bicycle. To keep your balance, you must keep moving.");
-        pprint("Success is not final, failure is not fatal: It is the courage to continue that counts.");
-        pprint("Happiness depends upon ourselves.");
-        pprint("The cat sat on the mat.");
-        pprint("Tomorrow is another day.");
-        pprint("Coding is fun and challenging.");
-        pprint("Music soothes the soul.");
-        pprint("The sun sets in the west.");
+        // pprint("The quick brown fox jumps over the lazy dog.");
+        // pprint("In the midst of chaos, there is also opportunity.");
+        // pprint("Life is like riding a bicycle. To keep your balance, you must keep moving.");
+        // pprint("Success is not final, failure is not fatal: It is the courage to continue that counts.");
+        // pprint("Happiness depends upon ourselves.");
+        // pprint("The cat sat on the mat.");
+        // pprint("Tomorrow is another day.");
+        // pprint("Coding is fun and challenging.");
+        // pprint("Music soothes the soul.");
+        // pprint("The sun sets in the west.");
 
         // some story ...
 
