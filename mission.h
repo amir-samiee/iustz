312879ad421437a31fcfcd0fc4777d7a83645643
Zinab Kaviani <152513893+zinabkaviani@users.noreply.github.com>
2024-03-18 12:21:26 +0330

    mission header without getter setter

--- mission.h
+++ mission.h
@@ -1,2 +1,73 @@
 #pragma once
 #include "headers.h"
+
+class Factory ;
+
+class Mission {
+
+    protected:
+
+        string missionName ;
+        int missionNum ;
+        int casualEnemyNum ;
+        int specialEnemyNum ;
+        string story ;
+        Factory* enemyBank ;
+        Storage inventory ;
+        Player* player ;
+    
+    public:
+
+    // getters:
+
+
+    // setters :
+
+
+    //methods:
+        void playerTurn();
+        bool endTurn();
+        void enemyTurn();
+        void endWave();
+        void removeDead();
+        void playerDeath();
+        void endMission();
+
+};
+
+
+class ZombieMission : public Mission {
+
+};
+
+class HumanMission : public Mission{
+
+};
+
+class Factory {
+    protected:
+        vector<vector<Character*>> enemies ;
+        Mission mission ;
+
+    public:
+    // getters:
+
+    // setters:
+
+    //methods:
+
+       vector<int> getWave();
+       virtual void createEnemy() = 0 ;
+
+};
+
+class ZombieFactory : public Factory{
+    public:
+        void createEnemy() override ;
+};
+
+class HumanFactory : public Factory {
+    public:
+    void createEnemy() override ;
+};
+
