6310827a7fdebd4d4809b7e4a8227dfa29a03275
Amir Hossein Samiee <celmir4444@gmail.com>
2024-03-19 15:14:53 +0330

    Update character.cpp

--- character.cpp
+++ character.cpp
@@ -18,7 +18,7 @@ int Stat::level()
 
 /// @brief ////////////////////////////////////////////////////
 
-int Character::level() { return firearmLevel + meleeLevel + hp.level() + stamina.level(); }
+int Character::level() { return (firearmLevel + meleeLevel + hp.level() + stamina.level()) / 4; }
 void Character::takeDamage(int newPoint)
 {
     if (newPoint <= 0)
