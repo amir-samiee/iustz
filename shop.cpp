d242881cfad9f233978272d23c84b010fdebbbf2
Zinab Kaviani <152513893+zinabkaviani@users.noreply.github.com>
2024-03-28 15:38:10 +0330

    throwable  logic added in shop

--- shop.cpp
+++ shop.cpp
@@ -7,24 +7,34 @@ void Shop :: displayShop() {
     
     cout << "-------------------------------------------------------------\n";
     cout << "Welcome to the shop! Here are the items available for purchase:\n";
-    cout << "1-Throwables\n2-Potions\n3-Weapon\n4-back" ;
+    int firstInput = getInput(options , 1 , 4 , true , "invalid input") ;
     cout << "-------------------------------------------------------------\n";
     
     int firstInput = getInput(options , 1 , 4 , true , "invalid input") ;
     int lastInput ;
+    string option2 ;
     string backOption = "0-Back\n";
      switch (firstInput) {
         case 1:
             // Handle Throwables menu
-          
+            cout << "Throwables Items : \n";
+            for (int i = 0; i <throwables.size(); ++i)
+            cout << i + 1 << "- Name: " << throwables[i]->getName() << " Price: " << throwables[i]->getPrice() << " Stamina: " << throwables[i]->getStamina() << " Damage: " << throwables[i]->getSpecial() << endl;
+            cout << backOption;
+            lastInput = getInput(backOption, 0, throwables.size(), true, "Invalid input");
+            if (lastInput == 0) {
+                displayShop(); // Go back to main menu
+            }
+            else if (lastInput > 0 && lastInput <= throwables.size()) {
+               player1->getBackpack()->addItem(throwables[lastInput - 1]->getName());
+                cout << "Item added to your backpack!\n";
+            }
             break;
         case 2:
-            // Handle Potions menu
-            cout << "Potions Items\n";
-            break;
+          // Handle Potions menu
         case 3:
             // Handle Weapon menu
-            cout << "Weapon Menu\n";
+            cout << "Permenet weapons Items\n";
            
             break;
         case 4:
