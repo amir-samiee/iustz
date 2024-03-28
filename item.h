5723043ee77bfb774173c218771731a03bd3fbd2
hamta rahmani <hamtar693@gmail.com>
2024-03-28 23:08:18 +0330

    update item.h

--- item.h
+++ item.h
@@ -190,14 +190,14 @@ Throwable throwable10("Gunship", 500, nullptr, 450, 900);
 Melee melee0("Punch", 0, nullptr, 0, 5);
 Melee melee1("Knife", 10, nullptr, 5, 15);
 Melee melee2("Axe", 10, nullptr, 10, 20);
-Melee melee3("Nunchaku", 20, nullptr, 20, 40);
-Melee melee4("Sword", 20, nullptr, 20, 40);
-Melee melee5("Katana", 30, nullptr, 30, 60);
-Melee melee6("Dagger", 30, nullptr, 30, 60);
-Melee melee7("Spear", 40, nullptr, 40, 80);
-Melee melee8("Halberd", 40, nullptr, 40, 80);
-Melee melee9("Rapier", 45, nullptr, 45, 90);
-Melee melee10("Chainsaw", 50, nullptr, 50, 100);
+Melee melee3("Nunchaku", 20, nullptr, 15, 30);
+Melee melee4("Sword", 20, nullptr, 15, 30);
+Melee melee5("Katana", 30, nullptr, 20, 40);
+Melee melee6("Dagger", 30, nullptr, 20, 40);
+Melee melee7("Spear", 40, nullptr, 25, 50);
+Melee melee8("Halberd", 40, nullptr, 25, 50);
+Melee melee9("Rapier", 45, nullptr, 25, 50);
+Melee melee10("Chainsaw", 50, nullptr, 30, 60);
 
 //"fireArm"
 Firearm fireArm1("Desert Eagle", 10, nullptr, 10, 20);
